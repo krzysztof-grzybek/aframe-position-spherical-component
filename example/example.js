1b6706f360052a989af9b03e34ddd2437de93a03
krzysztof-grzybek <krzysztof.grzybek89@gmail.com>
2018-06-14 22:42:15 +0200

    example improvements

--- example/example.js
+++ example/example.js
@@ -3,11 +3,14 @@ require('../index');
 
 AFRAME.registerComponent('earth', {
   init: function() {
-    this.initialPosition = [8, 1.57, 3.14];
+    this.initialPosition = [12, 1.57, 3.14];
+    this.initialRotation = [0, 0, 0];
   },
 
   tick: function() {
-    this.initialPosition[2] += 0.01;
+    this.initialPosition[2] += 0.005;
+    this.initialRotation[0] -= 0.05;
     this.el.setAttribute('position-spherical', this.initialPosition.join(' '));
+    this.el.setAttribute('rotation', this.initialRotation.join(' '));
   }
 })
